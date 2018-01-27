6b985b35087d0a6f8b3b86df2fd593aefb2d33a7
June-xa <june.yu@yamibuy.com>
2018-01-27 14:35:02 +0800

    修改filter modal界面、交互

--- app-web/src/main/webapp/org/cboard/controller/utils/paramSelector.js
+++ app-web/src/main/webapp/org/cboard/controller/utils/paramSelector.js
@@ -1,13 +1,64 @@
 /**
  * Created by yfyuan on 2017/5/2.
  */
-cBoard.controller('paramSelector', function ($scope, $uibModalInstance, dataService, param, filter, getSelects, ok) {
-    $scope.type = ['=', '≠', '>', '<', '≥', '≤', '(a,b]', '[a,b)', '(a,b)', '[a,b]'];
+cBoard.controller('paramSelector', function ($timeout, $scope, $uibModalInstance, dataService, param, filter, getSelects, ok) {
+    $scope.type = [
+        {
+            title: '等于',
+            value: '='
+        },
+        {
+            title: '不等于',
+            value: '≠'
+        },
+        {
+            title: '大于',
+            value: '>'
+        },
+        {
+            title: '小于',
+            value: '<'
+        },
+        {
+            title: '大于等于',
+            value: '≥'
+        },
+        {
+            title: '小于等于',
+            value: '≤'
+        },
+        {
+            title: '大于下限小于等于上限',
+            value: '(a,b]'
+        },
+        {
+            title: '大于等于下限小于上限',
+            value: '[a,b)'
+        },
+        {
+            title: '大于下限小于上限',
+            value: '(a,b)'
+        },
+        {
+            title: '大于等于下限小于等于上限',
+            value: '[a,b]'
+        },
+    ];
     $scope.param = param;
     $scope.operate = {};
     $scope.filter = filter;
     $scope.byFilter = {a: false};
     $scope.loadSelect = true;
+    $scope.showRange = false;
+    var filterFlag = null;
+    $scope.rangeItem = {
+        selected: '',
+        capped: '',
+        lowerLimit: ''
+    };
+    // 初始进来需要设置默认值 TODO （PS：属性类型一行需要隐藏）
+    $scope.selectedAttrKey = null;
+
     $scope.getSelects = function () {
         $scope.loading = true;
         getSelects($scope.byFilter.a, $scope.param.col, function (d) {
@@ -25,15 +76,18 @@ cBoard.controller('paramSelector', function ($scope, $uibModalInstance, dataServ
     };
     showValues();
     $scope.dbclickPush = function (o) {
+        // = ≠
         if ($scope.operate.equal) {
             if($scope.param.values.length == 1 && (_.isUndefined($scope.param.values[0]) || $scope.param.values[0]=='')){
                 $scope.param.values.length = 0;
             }
             $scope.param.values.push(o);
         }
+        // > < ≥ ≤
         if ($scope.operate.openInterval) {
             $scope.param.values[0] = o;
         }
+        // (a,b]...
         if ($scope.operate.closeInterval) {
             if ($scope.param.values[0] == undefined || $scope.param.values[0] == '') {
                 $scope.param.values[0] = o;
@@ -65,8 +119,19 @@ cBoard.controller('paramSelector', function ($scope, $uibModalInstance, dataServ
         return _.indexOf($scope.param.values, v) == -1;
     };
     $scope.filterType = function () {
+        var rang = ['(a,b]', '[a,b)', '(a,b)', '[a,b]'];
+        if($.inArray(param.type,rang) != -1){
+            // 显示上下限选择框
+            $scope.showRange = true;
+        }else {
+            $scope.showRange = false;
+        }
+
+        $scope.rangeItem.capped = '';
+        $scope.rangeItem.lowerLimit = '';
+
         $scope.param.values = [];
-        $scope.param.values.length = 1;
+        $scope.param.values.length = 0;
         showValues();
     };
     $scope.close = function () {
@@ -86,4 +151,120 @@ cBoard.controller('paramSelector', function ($scope, $uibModalInstance, dataServ
             $scope.param.values.length = 1;
         }
     };
+    // 选定日期类型
+    $scope.addToSelected = function () {
+        console.log(param.values);
+    }
+    // 选定时间范围
+    $scope.selectRange = function (type) {
+        if(type =='equal'){
+            if($.inArray($scope.rangeItem.selected,param.values) > -1){
+                $scope.exist_o = true;
+                $timeout(function(){
+                    $scope.exist_o = false;
+                },1500);
+                return;
+            }
+            param.values.push($scope.rangeItem.selected);
+        }else if(type == 'openInterval'){
+            param.values[0] = $scope.rangeItem.selected;
+        }else if(type == 'closeInterval'){
+            if(param.values[0] && (param.values[0] == $scope.rangeItem.capped)){
+                $scope.exist_t = true;
+                $timeout(function() {
+                    $scope.exist_t = false;
+                }, 1500);
+            }
+            if(param.values[1] && (param.values[1] == $scope.rangeItem.lowerLimit)){
+                $scope.exist_e = true;
+                $timeout(function() {
+                    $scope.exist_e = false;
+                }, 1500);
+            }
+
+            param.values = [];
+            param.values.push($scope.rangeItem.capped);
+            param.values.push($scope.rangeItem.lowerLimit);
+        }else if(type == 'qorw'){
+            if(param.type == '=' || param.type == '≠'){
+                param.values.push($scope.rangeItem.selected);
+            }else if(param.type == '>' || param.type == '<' || param.type == '≥' || param.type == '≤'){
+                param.values = [];
+                param.values[0] = $scope.rangeItem.selected;
+            }else {
+                param.values[0] = $scope.rangeItem.capped;
+                param.values[1] = $scope.rangeItem.lowerLimit;
+            }
+        }
+    }
+    //
+    $scope.clearValue = function (flag) {
+        if(flag == 1){
+            $scope.type = [
+                {
+                    title: '等于',
+                    value: '='
+                },
+                {
+                    title: '不等于',
+                    value: '≠'
+                },
+                {
+                    title: '大于',
+                    value: '>'
+                },
+                {
+                    title: '小于',
+                    value: '<'
+                },
+                {
+                    title: '大于等于',
+                    value: '≥'
+                },
+                {
+                    title: '小于等于',
+                    value: '≤'
+                },
+                {
+                    title: '大于下限小于等于上限',
+                    value: '(a,b]'
+                },
+                {
+                    title: '大于等于下限小于上限',
+                    value: '[a,b)'
+                },
+                {
+                    title: '大于下限小于上限',
+                    value: '(a,b)'
+                },
+                {
+                    title: '大于等于下限小于等于上限',
+                    value: '[a,b]'
+                },
+            ];
+        }else {
+            $scope.type = [
+                {
+                    title: '等于',
+                    value: '='
+                },
+                {
+                    title: '不等于',
+                    value: '≠'
+                }
+            ];
+        }
+        if(flag != filterFlag){
+            param.values = [];
+        }
+        filterFlag = flag;
+    }
+    // 删除当前选定值
+    $scope.clearSelected = function (index) {
+        if($scope.showRange){
+            param.values = [];
+        }else {
+            param.values.splice(index, 1);
+        }
+    }
 });
