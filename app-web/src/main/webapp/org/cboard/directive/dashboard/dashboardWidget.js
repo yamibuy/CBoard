d0f504abbde80e041b55d0dd1fba9378eb53a614
June-xa <june.yu@yamibuy.com>
2018-02-02 17:31:20 +0800

    调整UI

--- app-web/src/main/webapp/org/cboard/directive/dashboard/dashboardWidget.js
+++ app-web/src/main/webapp/org/cboard/directive/dashboard/dashboardWidget.js
@@ -6,7 +6,7 @@ cBoard.directive('dashboardWidget', function ($compile, $templateCache, dataServ
 
     var renderEchart = function (scope, element, attrs) {
         var template = $templateCache.get("echartContent");
-        scope.myheight = scope.row.height ? (scope.row.height) : 300;
+        scope.myheight = scope.row.height ? (scope.row.height - 44) : 400;
         var link = $compile(template);
         element.append(link(scope));
         var ndWrapper = $(element).find('.box-body');
@@ -15,7 +15,7 @@ cBoard.directive('dashboardWidget', function ($compile, $templateCache, dataServ
 
     var renderMap = function (scope, element, attrs) {
         var template = $templateCache.get("chartContent");
-        scope.myheight = scope.row.height ? (scope.row.height) : 400;
+        scope.myheight = scope.row.height ? (scope.row.height - 44 ) : 400;
         var link = $compile(template);
         element.append(link(scope));
         var ndWrapper = $(element).find('.box-body');
@@ -32,7 +32,7 @@ cBoard.directive('dashboardWidget', function ($compile, $templateCache, dataServ
 
     var renderTable = function (scope, element, attrs) {
         var template = $templateCache.get("chartContent");
-        scope.myheight = scope.row.height ? (scope.row.height) : 350;
+        scope.myheight = scope.row.height ? (scope.row.height - 44 ) : 400;
         var aa = $compile(template)(scope);
         element.append(aa);
         var ndWrapper = $(element).find('.box-body');
