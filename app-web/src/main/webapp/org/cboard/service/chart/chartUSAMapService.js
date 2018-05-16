89a4e8c8f51d3c5e90a4a45b1fd2b6294e7ea85e
royqi <roy.zhang@yamibuy.com>
2018-05-16 21:12:14 +0800

    修改美国地图位置

--- app-web/src/main/webapp/org/cboard/service/chart/chartUSAMapService.js
+++ app-web/src/main/webapp/org/cboard/service/chart/chartUSAMapService.js
@@ -238,7 +238,23 @@ cBoard.service('chartUSAMapService', function () {
             async: false,
             //type:'json',
             success: function (cityJson) {
-                echarts.registerMap(code, cityJson);
+                echarts.registerMap(code, cityJson,{
+                    Alaska: {              // 把阿拉斯加移到美国主大陆左下方
+                        left: -131,
+                        top: 25,
+                        width: 15
+                    },
+                    Hawaii: {
+                        left: -110,        // 夏威夷
+                        top: 28,
+                        width: 5
+                    },
+                    'Puerto Rico': {       // 波多黎各
+                        left: -76,
+                        top: 26,
+                        width: 2
+                    }
+                });
                 mapOption = {
                     legend: {
                         orient: 'vertical',
